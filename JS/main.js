1b11aa394f61dc9699bc5bca188fc7e4b568b5db
TudorPopesc <66331277+TudorPopesc@users.noreply.github.com>
2021-05-27 14:08:13 +0300

    update

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,31 @@
-let elem = document.querySelector('.main-block')
+let elem = document.querySelector('.main-block');
+const modal = document.querySelector('.modal_link');
+const closeModal = document.querySelector('.btn-close');
+const image = document.querySelector('.imag');
+const btn = document.querySelector('.btn-primary');
+let target;
+
 
 elem.addEventListener('click', function(e){
-	const target = e.target;
-	console.log(target.value)
+	let target = e.target;
+	
+	target.addEventListener('click', ()=>{
+	modal.classList.remove('hide');
 })
+	/*let value = 'photo ('+target.value+')'+'.jpg';
+	image.src = value;*/
+})
+
+
+modal.addEventListener('click' , function(e){
+     const target = e.target;
+     if(target == modal || target == closeModal){
+     	modal.classList.add('hide');
+     }  
+});
+document.addEventListener('keydown' ,  function(event) {
+	const key = event.key;
+	if(key == 'Escape'){
+		modal.classList.add('hide');
+	}
+});
