1ba81a26b75c98df12364dcaf91e50c6d4edbfcd
TudorPopesc <66331277+TudorPopesc@users.noreply.github.com>
2021-05-27 11:59:00 +0300

    Writen JS

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,6 @@
-'use strict'
-let img = document.getElementById("answer")
-let btnfind = document.querySelector('#find')
+let elem = document.querySelector('.main-block')
 
-btnfind.addEventListener('click', () => {
-
-	img.removeAttribute('src');
-	let inQuestion = document.querySelector('#question').value;
-	let notfound = document.querySelector('#nfound')
-
-	console.log(inQuestion)
-	switch (inQuestion) {
-		case 'Regulile de elaborare și aprobare a bugetului se numesc':
-			img.setAttribute("src", "IMG/photo1.jpg");
-			notfound.innerHTML = ''
-			break;
-		case 'Repartiția funcțională completează procesul repartiției prin intervenția administrațiilor publice determinînd formarea unor noi venituri':
-			img.setAttribute("src", "IMG/photo2.jpg");
-			notfound.innerHTML = ''
-			break;
-		default:
-			notfound.innerHTML = 'Nihuea nui'
-			break;
-	}
+elem.addEventListener('click',function(e){
+	const target = e.target;
+	console.log(target)
 })
